12138d1ee147223e1c0b9e186a2e014fc0af5f6c
SadiaShitol <shitol3080@gmail.com>
2023-09-20 14:15:30 +0600

    Front-End Update

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,11 +5,17 @@ function Buttons() {
     
     return (
         <>
-            <p>Are you a/an.......?</p>
-            <p>
-                <button className={styles.button}>Farmer</button>
-                <button className={styles.button}>Agronomist</button>
-            </p>
+        <div >
+        <p className={styles.FontText}>Are you a/an.......?</p>
+            
+            <ul className={styles.horizontal} type="none">
+               <li><button className={styles.button1}>Agronomist</button></li>
+               <li><button className={styles.button2} >Farmer</button></li>
+            </ul>
+            
+        </div>
+          
+            
         </>
     );
 }
