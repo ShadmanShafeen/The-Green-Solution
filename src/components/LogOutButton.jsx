0337629c91113fa445c0e08fa8aac48e4474cee0
SadiaShitol <shitol3080@gmail.com>
2023-10-01 11:49:05 +0600

    fixing merge

--- src/components/LogOutButton.jsx
+++ src/components/LogOutButton.jsx
@@ -1,20 +1,19 @@
+import { Link } from 'react-router-dom';
 import styles from './LogOutButton.module.css'
+import { motion } from 'framer-motion'
 
 function LogOutButton ()
 {
     return(
-       
+        <Link to ='/'>
+        <motion.button className={styles.LogOutButton} onClick={null}
+    whileHover={{scale: 1.2}}
+    whileTap={{scale: 0.9}}>
+      Log Out
+    </motion.button>   
 
-       
-              <button className={styles.LogOutButton}>
-           
-         Log Out
-        </button>
-    
-       
-        
-       
-    
+        </Link>
+     
     )
 }
  
