d10497129e170dc5b176f6bbf4aa44a865510353
Shadman Shafeen Khan <shadmanskystar@gmail.com>
2023-12-03 17:29:22 +0600

    Fixing base url for production

--- src/CONSTANT.js
+++ src/CONSTANT.js
@@ -1,3 +1,3 @@
 const BASE_URL = `https://the-green-solution-backend.onrender.com`
+// const BASE_URL = `http://localhost:5000`
 export default BASE_URL;
-// export default BASE_URL = `http://localhost:5173`
